181d63d6efd79bb64b8eaaa9de4e2a8111c20b54
samsquareed <samsquare.professional@gmail.com>
2022-05-04 23:11:37 +0530

    notice well man : API endpoints must be same on both front and backend

--- src/api/index.js
+++ src/api/index.js
@@ -4,5 +4,5 @@ const API =  Axios.create({baseURL : 'http://localhost:8080'});
 
 
 
-export const signIn = (formData) => API.post('/user/signin', formData);
-export const signUp = (formData) => API.post('/user/signup', formData);
+export const signIn = (formData) => API.post('/auth/signin', formData);
+export const signUp = (formData) => API.post('/auth/signup', formData);
