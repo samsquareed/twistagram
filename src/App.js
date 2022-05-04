0d2bf8e5e77e95366ab7fefb858a2b9277fa4331
samsquareed <samsquare.professional@gmail.com>
2022-05-05 00:11:09 +0530

    first greet with login then move to home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
@@ -8,12 +8,14 @@ import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("user"))
+
   return (
     <Router>
       <Box>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          <Route exact path='/' element={ user ? <Home /> : <Login />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
         </Routes>
