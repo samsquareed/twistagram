9700624c060f50d4d9dfc00662ab80cb0575af18
samsquareed <samsquare.professional@gmail.com>
2022-05-04 15:40:29 +0530

    left bar customized and other

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -1,10 +1,85 @@
-import { Box } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import React from 'react'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import {
+  AccountBox,
+  Article,
+  Group,
+  Home,
+  ModeNight,
+  Person,
+  Settings,
+  Storefront,
+} from "@mui/icons-material";
 
 const Leftbar = () => {
   return (
     <Box bgcolor="whitesmoke" flex={2} p={2} sx={{display:{xs:"none", sm: "block"}}}>
-      leftbar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton href='#'>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText> Home</ListItemText>
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton href='#'>
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText> Profile</ListItemText>
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton href='#'>
+            <ListItemIcon>
+              <Article />
+            </ListItemIcon>
+            <ListItemText> Pages</ListItemText>
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton href='#'>
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText> contacts</ListItemText>
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton href='#'>
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText> store</ListItemText>
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton href='#'>
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText> settings</ListItemText>
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton href='#'>
+            <ListItemIcon>
+              <ExpandMoreIcon />
+            </ListItemIcon>
+            <ListItemText> More</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   )
 }
