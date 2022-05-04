2dc7a1d905335c067d9a4464552a18b383396eb5
samsquareed <samsquare.professional@gmail.com>
2022-05-05 00:24:21 +0530

    amazing trick to solve the problem : greet with login page before home page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import React, { useEffect, useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 
+const LogicalLoginHome = () =>{
+  const nevigate = useNavigate()
+
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    if(!user)
+      nevigate("/login")
+  }, [])
+  
+  return (
+    <Routes>
+      <Route exact path='/' element={ <Home /> } />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  )
+
+}
+
+
 const App = () => {
   const user = JSON.parse(localStorage.getItem("user"))
-
+  
   return (
     <Router>
       <Box>
         <Navbar />
-        <Routes>
-          <Route exact path='/' element={ user ? <Home /> : <Login />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
+        <LogicalLoginHome />
     </Box>
     </Router>
   )
